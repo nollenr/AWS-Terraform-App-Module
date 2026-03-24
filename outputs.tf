370e0981acdc06df62f2c3d28a97ff7f0fc68205
nollenr <nollenr@gmail.com>
2026-03-24 14:41:00 -0700

    added items to outputs.tf for multi-region use

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
+output "vpc_id" {
+  description = "ID of the VPC created by the module"
+  value       = aws_vpc.main.id
+}
+
+output "security_group_intra_node_id" {
+  description = "ID of the security group allowing intra-node communication"
+  value = module.security-group-02.security_group_id
+}
+
 output "app_instance_ips" {
   description = "IP addresses of app instance"
   value = length(aws_instance.app) > 0 ? {
