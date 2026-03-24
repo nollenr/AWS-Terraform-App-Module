364950eba2691d878864fed895089aa1c5250bdb
nollenr <nollenr@gmail.com>
2026-03-24 14:45:22 -0700

    added items to outputs.tf for multi-region use

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "security_group_intra_node_id" {
   value = module.security-group-02.security_group_id
 }
 
+output "route_table_public_id" {
+  description = "ID of the public route table"
+  value = aws_route_table.public_route_table.id
+}
+
 output "app_instance_ips" {
   description = "IP addresses of app instance"
   value = length(aws_instance.app) > 0 ? {
